c2d66f7ba952e94e74b2a059988a7e9c413d6e3a
Chathura Siriwardhana <chathuram@codegen.net>
2019-06-06 16:38:57 +0530

    email queue

--- src/main/java/server/EmailSender.java
+++ src/main/java/server/EmailSender.java
@@ -2,59 +2,88 @@ package server;
 
 import model.SendEmailRequest;
 
-import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class EmailSender
+public class EmailSender implements Runnable
 {
-    //email queue
-    //add to queue
-    //get fom queu and send
-    //email settings
-    //validate email address before accept
-	Session session;
+	private LinkedBlockingQueue<SendEmailRequest> mailQueue;
+	private boolean run;
+	//email queue
+	//add to queue
+	//get fom queu and send
+	//email settings
+	//validate email address before accept
+	private Session session;
 
 	final String SENDER_ADDRESS = "chathura@mymail.org";
 
 	public void init()
 	{
-		Properties properties=new Properties();
-		properties.put( "mail.smtp.host","localhost" );
-		properties.put( "mail.smtp.port","25" );
-		properties.put( "mail.smtp.auth","false" );
+		run = true;
+		mailQueue = new LinkedBlockingQueue<>();
+		Properties properties = new Properties();
+		properties.put( "mail.smtp.host", "localhost" );
+		properties.put( "mail.smtp.port", "25" );
+		properties.put( "mail.smtp.auth", "false" );
 		//fill all the information like host name etc.
-		session=Session.getDefaultInstance(properties,null);
+		session = Session.getDefaultInstance( properties, null );
 	}
 
-	public void sendMessage( SendEmailRequest emailRequest) throws MessagingException
+	public void sendMessage( SendEmailRequest emailRequest ) throws MessagingException
 	{
 
 		MimeMessage message = compose( session, emailRequest );
 		send( message );
-		System.out.println("email :"+emailRequest.getRequestId()+" sent");
+		System.out.println( "email :" + emailRequest.getRequestId() + " sent" );
 	}
 
-	private MimeMessage compose(Session session, SendEmailRequest emailRequest) throws MessagingException
+	public boolean queueMessage( SendEmailRequest emailRequest )
 	{
-		MimeMessage message=new MimeMessage(session);
-		message.setFrom(new InternetAddress(SENDER_ADDRESS));
+		return this.mailQueue.offer( emailRequest );
+	}
+
+	private MimeMessage compose( Session session, SendEmailRequest emailRequest ) throws MessagingException
+	{
+		MimeMessage message = new MimeMessage( session );
+		message.setFrom( new InternetAddress( SENDER_ADDRESS ) );
 		message.addRecipient( Message.RecipientType.TO,
-				new InternetAddress(emailRequest.getRecipientAddress()));
-		message.setSubject(emailRequest.getSubject());
-		message.setText(emailRequest.getMessage());
+				new InternetAddress( emailRequest.getRecipientAddress() ) );
+		message.setSubject( emailRequest.getSubject() );
+		message.setText( emailRequest.getMessage() );
 		return message;
 	}
 
-	private void send(MimeMessage mail) throws MessagingException
+	private void send( MimeMessage mail ) throws MessagingException
 	{
 		Transport.send( mail );
-		System.out.println("message sent successfully....");
+		System.out.println( "message sent successfully...." );
 	}
 
-
+	public void run()
+	{
+		while ( run )
+		{
+			try
+			{
+				SendEmailRequest request = mailQueue.take();
+				MimeMessage mail = compose( session, request );
+				send( mail );
+			}
+			catch ( InterruptedException e )
+			{
+				e.printStackTrace();
+			}
+			catch ( MessagingException e )
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/test/java/server/EmailSenderTest.java
+++ src/test/java/server/EmailSenderTest.java
@@ -7,6 +7,11 @@ import org.junit.Test;
 
 import javax.mail.MessagingException;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,19 +27,70 @@ public class EmailSenderTest
 	{
 		emailSender = new EmailSender();
 		emailSender.init();
+		System.out.println("email sender init");
 	}
 
 
 	@Test
-	public void send() throws MessagingException
+	public void sendTest() throws MessagingException
 	{
 		SendEmailRequest emailRequest = new SendEmailRequest(  );
 		emailRequest.setRequestId("req_id_01");
 		emailRequest.setSenderName("Chathura");
 		emailRequest.setRecipientAddress("siripala@gmail.com");
 		emailRequest.setSubject("Greetings");
-		emailRequest.setMessage("Hi siripala,How are you? \n thanks.");
+		emailRequest.setMessage("Hi siripala,How are you? \nthanks.");
 
 		emailSender.sendMessage(emailRequest);
+	}
+
+	@Test
+	public void emailQueueTest()
+	{
+		SendEmailRequest emailRequest = new SendEmailRequest(  );
+		emailRequest.setRequestId("req_id_01");
+		emailRequest.setSenderName("Chathura");
+		emailRequest.setRecipientAddress("siripala@gmail.com");
+		emailRequest.setSubject("Greetings");
+		emailRequest.setMessage("Hi siripala,How are you? \nthanks.");
+
+		emailSender.queueMessage( emailRequest );
+
+		SendEmailRequest emailRequest2 = new SendEmailRequest(  );
+		emailRequest2.setRequestId("req_id_01");
+		emailRequest2.setSenderName("Chathura");
+		emailRequest2.setRecipientAddress("saman@gmail.com");
+		emailRequest2.setSubject("Greetings");
+		emailRequest2.setMessage("Hi Saman Kumara,How are you? \nthanks.");
+
+		emailSender.queueMessage( emailRequest2 );
+
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+		executorService.execute( new Thread( emailSender ) );
+
+		SendEmailRequest emailRequest3 = new SendEmailRequest(  );
+		emailRequest3.setRequestId("req_id_01");
+		emailRequest3.setSenderName("Chathura");
+		emailRequest3.setRecipientAddress("john@gmail.com");
+		emailRequest3.setSubject("Greetings");
+		emailRequest3.setMessage("Hi john,How are you? \nthanks.");
+
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+		System.out.println("tasks scheduling");
+		executor.schedule(() -> emailSender.queueMessage( emailRequest3 ), 10, TimeUnit.SECONDS );
+		executor.schedule(() -> emailSender.queueMessage( emailRequest3 ), 20, TimeUnit.SECONDS );
+		executor.schedule(() -> emailSender.queueMessage( emailRequest3 ), 30, TimeUnit.SECONDS );
+
+
+		try
+		{
+			Thread.sleep( 60000 );
+		}
+		catch ( InterruptedException e )
+		{
+			e.printStackTrace();
+		}
 	}
 }
